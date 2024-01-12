72afd16149a39f272b5676ac71dbef1ecb5fefb3
kisaragui <njesusas@gmail.com>
2024-01-12 20:19:38 -0300

    subiendo modulo formulario de prueba

---
+++ addons/prueba_form/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Formulario de prueba',
+    'version' : '1.0',
+    'summary': 'probanndo',
+    'depends': ['base'],
+    'author': "Noel Alvarez",
+    'category': 'Extra Tools',
+    'description': """Probando formulario""",
+    'license': 'LGPL-3',
+    "installable": True,
+    "application": True,
+    'auto_install': False,
+}
