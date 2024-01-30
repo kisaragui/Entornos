912a4adfc78738dd4103c5ae6532756d2ca9a19d
kisaragui <njesusas@gmail.com>
2024-01-29 23:23:07 -0300

    probando grafico en componentes

---
+++ addons/graph_test/static/src/components/barChart/barchart.js
@@ -0,0 +1,12 @@
+/** @odoo-module **/
+import { registry } from "@web/core/registry"
+import { loadJS } from "@web/core/assets"
+import { Component, onWillStart, useRef, onMounted } from "@odoo/owl";
+
+export class BarChartComponent extends Component {
+    static template =  "graph_test.graph_barchart";
+
+}
+
+
+
